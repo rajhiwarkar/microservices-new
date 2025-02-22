8de58a670177964e52725cf9dca1b9dbd6d5a86b
hiwarkar <66463030+rajhiwarkar@users.noreply.github.com>
2025-02-22 13:58:59 +0530

    ubuntu_commits (#1)
    * feat: fixing addition of inventory whenever starting application
    
    * feat: adding eureka implementation

--- inventory-service/src/main/java/com/programmingtechie/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programmingtechie/inventory_service/controller/InventoryController.java
@@ -1,24 +1,27 @@
 package com.programmingtechie.inventory_service.controller;
 
-import com.programmingtechie.inventory_service.service.InverntoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.programmingtechie.inventory_service.dto.InventoryResponse;
+import com.programmingtechie.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
 
-    @Autowired
-    private InverntoryService inverntoryService;
+    private final InventoryService inverntoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inverntoryService.inInStock(skuCode);
     }
 }
