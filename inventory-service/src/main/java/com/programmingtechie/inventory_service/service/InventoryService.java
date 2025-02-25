4c82c4641a95a84a435beff5b9ee921d0b4ab40d
hiwarkar <66463030+rajhiwarkar@users.noreply.github.com>
2025-02-25 15:27:54 +0530

    ubuntu_commits (#6)
    * feat: fixing addition of inventory whenever starting application
    
    * feat: adding eureka implementation
    
    * feat: adding missing files
    
    * feat: adding api gateway config
    
    * feat: adding git ignore file
    
    * feat: adding async call and implementing circuit breaker

--- inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/programmingtechie/inventory_service/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.programmingtechie.inventory_service.service;
 import com.programmingtechie.inventory_service.dto.InventoryResponse;
 import com.programmingtechie.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +11,14 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> inInStock(List<String> skuCode) {
+    public List<InventoryResponse> inInStock(List<String> skuCode) throws InterruptedException {
+
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
--- order-service/src/main/java/com/programmingtechie/order_service/controller/orderController.java
+++ order-service/src/main/java/com/programmingtechie/order_service/controller/orderController.java
@@ -2,12 +2,18 @@ package com.programmingtechie.order_service.controller;
 
 import com.programmingtechie.order_service.dto.OrderRequest;
 import com.programmingtechie.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("api/order")
@@ -18,9 +24,14 @@ public class orderController {
     private OrderService orderService;
 
     @PostMapping
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        log.info("Order Placed Successfully");
-        return "Order Placed Successfully";
+    @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please place order in some time!");
     }
 }
