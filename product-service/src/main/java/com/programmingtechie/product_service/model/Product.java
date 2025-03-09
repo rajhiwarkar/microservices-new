10787d1a2c704086787f3053f4a7ed5972ec111b
rajhiwarkar <hiwarkr@gmail.com>
2025-03-09 18:23:22 +0530

    feat: removing mongodb and jwt

--- product-service/src/main/java/com/programmingtechie/product_service/model/Product.java
+++ product-service/src/main/java/com/programmingtechie/product_service/model/Product.java
@@ -1,25 +1,26 @@
 package com.programmingtechie.product_service.model;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
-@Document(value = "product")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+
     private String name;
     private String description;
     private BigDecimal price;
-
 }
--- product-service/src/main/java/com/programmingtechie/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/programmingtechie/product_service/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.programmingtechie.product_service.repository;
 
 import com.programmingtechie.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
