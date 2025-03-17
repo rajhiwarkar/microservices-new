6b7e1a71d68a1efc88ae5a535cd169166b7335b7
rajhiwarkar <hiwarkr@gmail.com>
2025-03-17 10:49:29 +0530

    feat: adding mongodb again for product repo

--- product-service/src/main/java/com/programmingtechie/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/programmingtechie/product_service/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package com.programmingtechie.product_service.repository;
 
 import com.programmingtechie.product_service.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
